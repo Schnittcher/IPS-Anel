a6583f2b26c793c9e4d4ab5826c0ebba30272e1d
Kai <kai.schnittcher@gmail.com>
2020-08-17 11:17:37 +0200

    Discovery von GetConfigurationForm nach Niels seinem Beispiel entkapselt

--- AnelDiscovery/module.php
+++ AnelDiscovery/module.php
@@ -15,11 +15,19 @@ class AnelDiscovery extends IPSModule
     {
         //Never delete this line!
         parent::Create();
+
         $this->RegisterPropertyString('Username', 'admin');
         $this->RegisterPropertyString('Password', 'anel');
-        $this->SetBuffer('discoveredDevices', '{}');
+
+        // Only required for pre 5.5
+        if (floatval(IPS_GetKernelVersion()) < 5.5) {
+            $this->RegisterTimer('LoadDevicesTimer', 0, 'ANEL_LoadDevices($_IPS["TARGET"]);');
+        }
+
+        $this->SetBuffer('discoveredDevices', json_encode([]));
+        $this->SetBuffer('SearchActive', json_encode(false));
+
         $this->RegisterMessage(0, IPS_KERNELSTARTED);
-        $this->RegisterTimer('AnelDiscovery', 3600000, 'ANEL_discoverDevices($_IPS[\'TARGET\'])');
     }
 
     public function ApplyChanges()
@@ -34,33 +42,21 @@ class AnelDiscovery extends IPSModule
         //$Devices = $this->discoverDevices();
 
         $Devices = json_decode($this->GetBuffer('discoveredDevices'), true);
-        IPS_LogMessage('debug', print_r($Devices, true));
 
-        $Values = [];
+        if (!json_decode($this->GetBuffer('SearchActive'))) {
+            $this->SetBuffer('SearchActive', json_encode(true));
 
-        foreach ($Devices as $Device) {
-            $instanceID = $this->getAnelInstances($Device['IP']);
-
-            $AddValue = [
-                'IPAddress'             => $Device['IP'],
-                'name'                  => $Device['deviceName'],
-                'instanceID'            => $instanceID
-            ];
-
-            $AddValue['create'] = [
-                [
-                    'moduleID'      => '{13F0B37E-30C9-C043-A5AC-2D9B6A90E9F2}',
-                    'configuration' => [
-                        'IPAddress' => $Device['IP'],
-                        'Username'  => $this->ReadPropertyString('Username'),
-                        'Password'  => $this->ReadPropertyString('Password')
-                    ]
-                ],
-            ];
-
-            $Values[] = $AddValue;
+            // Start device search in a timer, not prolonging the execution of GetConfigurationForm
+            if (floatval(IPS_GetKernelVersion()) < 5.5) {
+                $this->SetTimerInterval('LoadDevicesTimer', 200);
+            } else {
+                $this->SendDebug('Start', 'OnceTimer', 0);
+                $this->RegisterOnceTimer('LoadDevicesTimer', 'ANEL_LoadDevices($_IPS["TARGET"]);');
+            }
         }
-        $Form['actions'][0]['values'] = $Values;
+
+        $Form['actions'][0]['visiable'] = count($Devices) == 0;
+        $Form['actions'][1]['values'] = $Devices;
         return json_encode($Form);
     }
 
@@ -104,7 +100,40 @@ class AnelDiscovery extends IPSModule
         } while (time() < $discoveryTimeout);
         socket_close($sock);
 
-        $this->SetBuffer('discoveredDevices', json_encode($discoveryList));
+        $Values = [];
+
+        foreach ($discoveryList as $Device) {
+            $instanceID = $this->getAnelInstances($Device['IP']);
+
+            $AddValue = [
+                'IPAddress'             => $Device['IP'],
+                'name'                  => $Device['deviceName'],
+                'instanceID'            => $instanceID
+            ];
+
+            $AddValue['create'] = [
+                [
+                    'moduleID'      => '{13F0B37E-30C9-C043-A5AC-2D9B6A90E9F2}',
+                    'configuration' => [
+                        'IPAddress' => $Device['IP'],
+                        'Username'  => $this->ReadPropertyString('Username'),
+                        'Password'  => $this->ReadPropertyString('Password')
+                    ]
+                ],
+            ];
+
+            $Values[] = $AddValue;
+        }
+
+        if (floatval(IPS_GetKernelVersion()) < 5.5) {
+            $this->SetTimerInterval('LoadDevicesTimer', 0);
+        }
+        $this->SetBuffer('SearchActive', json_encode(false));
+        $this->LogMessage($this->Translate('Discovery progress done'), KL_NOTIFY);
+
+        $this->SetBuffer('discoveredDevices', json_encode($Values));
+        $this->UpdateFormField('configurator', 'values', $Values);
+        $this->UpdateFormField('searchingInfo', 'visible', false);
         return;
     }
 
