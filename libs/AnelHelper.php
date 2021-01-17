d2bf0fd07830740a75906e0c4a2478a8a149ca02
Kai Schnittcher <kai.schnittcher@gmail.com>
2021-01-17 10:55:12 +0100

    Fix für SymBox

---
+++ libs/AnelHelper.php
@@ -0,0 +1,12 @@
+<?php
+if (!function_exists('fnmatch')) {
+    define('FNM_PATHNAME', 1);
+    define('FNM_NOESCAPE', 2);
+    define('FNM_PERIOD', 4);
+    define('FNM_CASEFOLD', 16);
+
+    function fnmatch($pattern, $string, $flags = 0)
+    {
+        return pcre_fnmatch($pattern, $string, $flags);
+    }
+}
